a33a5ae40c36605ed49d5d5bbc9fbfe47b75a31a
Harshal patil <harshalpatil8482@gmail.com>
2024-07-24 01:42:56 +0530

    calculator created using json

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def home():
     return "This is Home page"
 
+@app.route("/prime")
+def cal_prime():
+    num = request.json["num"]
+    l1 =[]
+    for i in range(2,num+1):
+        if num > 1:
+            for j in range(2,i):
+                if i % j == 0:
+                    break
+            else:
+                l1.append(i)
+
+    return str(l1)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
